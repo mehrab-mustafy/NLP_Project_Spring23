b110148b3aec8a1a9866269e03b36bcc2f359ffd
mehrab-mustafy <nibir1705@gmail.com>
2024-04-14 00:02:08 -0500

    spellcheck completed 14/4/24 12:01am

--- run_project.py
+++ run_project.py
@@ -10,15 +10,54 @@ from sample_code_1 import num_sentences, spelling_mistakes
 from sample_code_2 import agreement, verbs
 
 
+def read_file_contents(filename, directory):
+    file_path = os.path.join(directory, filename)
+    with open(file_path, 'r') as file:
+        return file.read()
+    
+
 def main():
     
     #this_input = input("Enter the Essay to grade: ")
-    text = 'Finaly, people were being successful or not is depen on what is your personal atetivies. Somehow people sucessful just for their own good, but many of other successful people I think will probably pay back for the socity, so i still believed people suessful is not just for thier own good is for every one in the would.'
+
+    df = pd.read_csv('index.csv', delimiter=';')
+    directory = 'essays/'        
+    df['file_contents'] = df.apply(lambda row: read_file_contents(row['filename'], directory), axis=1)
+    df['file_contents'] = df['file_contents'].str.replace('\n', '').str.replace('\t', '').str.replace("'", '')
+    df['file_contents'] = df['file_contents'].str.replace(r'\s+', ' ', regex=True)
+    # df['num_mistakes']  = df['file_contents'].apply(spelling_mistakes)    
+    # max_mistakes, min_mistakes = df['num_mistakes'].max(), df['num_mistakes'].min()
+    # print(df.columns)
+    # print(max_mistakes, min_mistakes)
+    
+    # high_grade_df = df[df['grade'] == 'high']
+    # high_grade_df['num_mistakes'] = high_grade_df['file_contents'].apply(spelling_mistakes)
+    # max_mistakes1, min_mistakes1 = high_grade_df['num_mistakes'].max(), high_grade_df['num_mistakes'].min()
+
+    
+    # low_grade_df = df[df['grade'] == 'low']
+    # low_grade_df['num_mistakes'] = low_grade_df['file_contents'].apply(spelling_mistakes)
+    # max_mistakes2, min_mistakes2 = low_grade_df['num_mistakes'].max(), low_grade_df['num_mistakes'].min()
+
+    # print(max_mistakes1, min_mistakes1)
+    # print(max_mistakes2, min_mistakes2)
+    
+    text = df.loc[0, 'file_contents']
+    text = 'I am writing I am well'
+    print(text)
     a = num_sentences(text)
+    print(a)
     b = spelling_mistakes(text)
+    print(b)
+    # s1 = 'Jessica is 8 years old'
+    # s2 = 'The goats are eating grass'
+    # tags1 = num_sentences(s1)
+    # tags2 = num_sentences(s2)
+    # print(s1, tags1)
+    # print(s2, tags2)
     # c_1, c_2, c_3 = agreement()
     # d_1, d_2 = verbs()
-    print(a, b)
+    # print(a, b)
     # Final_Score = 2*a - b + c_1 + c_2 + 2*c_3 + 3*d_1 + 2*d_2
     # print(f"The final score of this essay is: {Final_Score}")
     # print("This essay is classified as: high")
--- sample_code_1.py
+++ sample_code_1.py
@@ -1,30 +1,43 @@
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk import pos_tag
 from nltk.corpus import words
 nltk.download('punkt')
 nltk.download('words')
 nltk.download('averaged_perceptron_tagger')
-import string
+import spacy
+from spellchecker import SpellChecker
+nlp = spacy.load("en_core_web_sm")
+
+
 
 def num_sentences(text):
+    text = 'I eating a sandwich'
     tokens = nltk.word_tokenize(text)
     pos_tags = nltk.pos_tag(tokens)
     pos_tags_list = [tag[1] for tag in pos_tags]
+    
+    
     return pos_tags_list
 
 
-
-def remove_punctuation(sentence):
-    sentence_without_punct = ""
-    for char in sentence:
-        if char not in string.punctuation:
-            sentence_without_punct += char
-    return sentence_without_punct
-
+spell = SpellChecker()
 def spelling_mistakes(text):
-    text_without_punct = remove_punctuation(text)
-    tokens = word_tokenize(text_without_punct)
-    english_vocab = set(words.words())
-    misspelled_words = [word for word in tokens if word.lower() not in english_vocab]
-    print(f"misspelled words: {misspelled_words}")
-    return len(misspelled_words)
+    doc = nlp(text)
+    misspelled_words = []
+    for token in doc:
+        if not token.is_punct and not token.is_stop:
+            if token.text.lower() not in spell:
+                misspelled_words.append(token.text)
+    # print(f"Misspelled words: {misspelled_words}")
+    # print(f"Number of misspelled words: {len(misspelled_words)}")
+    if(len(misspelled_words) <= 5):
+        return 0
+    elif(len(misspelled_words) > 5 and len(misspelled_words) <= 15):
+        return 1
+    elif(len(misspelled_words) > 15 and len(misspelled_words) <= 25):
+        return 2
+    elif(len(misspelled_words) > 25 and len(misspelled_words) <= 40):
+        return 3
+    else:
+        return 4 
